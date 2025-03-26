7eed92351e27ecf3546b315424a595466ed34d9b
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2025-03-26 11:22:19 -0700

    Add automated DaisyUI build step

--- CruSibyl.Web/Controllers/AdminController.cs
+++ CruSibyl.Web/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CruSibyl.Web.Controllers;
 
+[Authorize]
 public class AdminController : TabController
 {
     public IActionResult Index() => HandleTabRequest("_Content");
