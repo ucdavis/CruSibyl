b1b75461ff4a9415e5d21eeafd431aec41e3e3f6
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2025-07-25 16:50:32 -0700

    Reconfigure for a consumption plan

--- CruSibyl.Functions/ManifestSyncFunction.cs
+++ CruSibyl.Functions/ManifestSyncFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
 using CruSibyl.Core.Services;
+using Microsoft.Azure.Functions.Worker.Http;
 
 public class ManifestSyncFunction
 {
@@ -15,8 +16,18 @@ public class ManifestSyncFunction
     }
 
     [Function("ManifestSyncFunction")]
-    public async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo timer)
+    public async Task Run([TimerTrigger("0 0 8 * * *")] TimerInfo timer)
     {
         await _service.SyncManifestsAsync();
+    }
+
+    [Function("ManifestSyncFunction_Http")]
+    public async Task<HttpResponseData> RunHttp(
+        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+    {
+        await _service.SyncManifestsAsync();
+        var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+        await response.WriteStringAsync("Manifest sync triggered.");
+        return response;
     }
 }
--- CruSibyl.Functions/PackageVersionSyncFunction.cs
+++ CruSibyl.Functions/PackageVersionSyncFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
 using CruSibyl.Core.Services;
+using Microsoft.Azure.Functions.Worker.Http;
 
 public class PackageVersionSyncFunction
 {
@@ -18,5 +19,15 @@ public class PackageVersionSyncFunction
     public async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo timer)
     {
         await _service.SyncPackageVersionsAsync();
+    }
+
+    [Function("PackageVersionSyncFunction_Http")]
+    public async Task<HttpResponseData> RunHttp(
+        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+    {
+        await _service.SyncPackageVersionsAsync();
+        var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+        await response.WriteStringAsync("Package version sync triggered.");
+        return response;
     }
 }
